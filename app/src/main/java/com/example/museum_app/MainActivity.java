76477a6ade5e6b099c553d93a74c89d1a3f6ee44
Camille Leloutre <c_leloutre@stu-psbedu.paris>
2023-04-14 18:32:31 +0200

    Correctif de liens

--- app/src/main/java/com/example/museum_app/MainActivity.java
+++ app/src/main/java/com/example/museum_app/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.carrousel_activity);
+        setContentView(R.layout.menu_general);
     }
 }
