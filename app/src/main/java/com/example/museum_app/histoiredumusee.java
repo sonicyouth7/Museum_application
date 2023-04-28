7dfb1be8a2bdd73da140a33e03056d1e537348b8
marionlanglois <marion-langlois@hotmail.com>
2023-04-28 22:57:16 +0200

    histoiredumusee description_oeuvre

--- app/src/main/java/com/example/museum_app/histoiredumusee.java
+++ app/src/main/java/com/example/museum_app/histoiredumusee.java
@@ -1,22 +1,35 @@
-package com.example.museum_app;
+package com.example.yourapplication;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class histoiredumusee extends AppCompatActivity {
+import com.example.museum_app.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.histoiredumusee);
+
+        TextView videoTitle = findViewById(R.id.video_title);
+        videoTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openYoutubeVideo();
+            }
+        });
     }
 
-    public void openYoutubeVideo(View view) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=U-DOGB4iVBM"));
+    private void openYoutubeVideo() {
+        String videoId = "id_louvre"; // Remplacez ceci par l'ID de votre vidéo
+        String youtubeUrl = "https://www.youtube.com/watch?v=U-DOGB4iVBM" + "id_louvre";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(youtubeUrl));
         startActivity(intent);
     }
 }
