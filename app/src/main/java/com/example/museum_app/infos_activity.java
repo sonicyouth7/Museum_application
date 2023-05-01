f7ff4b5ba80f3b6700d90e31df2fa2f97437971e
marionlanglois <marion-langlois@hotmail.com>
2023-05-01 22:37:00 +0200

    histoiredumusee description_oeuvre

--- app/src/main/java/com/example/museum_app/infos_activity.java
+++ app/src/main/java/com/example/museum_app/infos_activity.java
@@ -1,4 +1,24 @@
 package com.example.museum_app;
 
-public class infos_activity {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class infos_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.infos);
+    }
+
+    public void onReserveNowClick(View view) {
+        String url = getString(R.string.reserve_now_link);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
 }
